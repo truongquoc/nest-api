42a3f5b8e38ea916544a3484d1fdbb8c80417b73
Truong Quoc <ngotruongquoc0102@gmail.com>
2020-07-28 15:00:03 +0700

    add validation pipe

--- src/App/auth/auth.controller.ts
+++ src/App/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes } from '@nestjs/common';
 import { AuthServices } from './auth.service';
 import { LoginDTO, RegisterDTO } from 'src/App/auth/auth.dto';
+import { ValidationPipe } from 'src/shared/validation.pipe';
 @Controller('api/v1/auth')
 export class AuthController {
   constructor(private authService: AuthServices) {}
@@ -11,10 +12,13 @@ export class AuthController {
   }
 
   @Post()
+  @UsePipes(new ValidationPipe())
   async Login(@Body() data: LoginDTO) {
     return this.authService.login(data);
   }
 
   @Post()
-  async Register(@Body() userDTO: RegisterDTO) {}
+  async Register(@Body() userDTO: RegisterDTO) {
+    return this.authService;
+  }
 }
