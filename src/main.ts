0b85b6f4b499c3d677dea11901a93df1edf2347e
Truong Quoc <ngotruongquoc0102@gmail.com>
2020-07-28 10:22:47 +0700

    config env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
-const port = process.env.PORT;
+const port = 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
@@ -14,7 +14,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(port);
   Logger.log(`Server running on htttp://localhost:${port}`);
 }
 bootstrap();
