0522bcbfae3fd8659971a8792158a032b8487046
Truong Quoc <ngotruongquoc0102@gmail.com>
2020-08-06 13:14:47 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
-<<<<<<< HEAD
-const port = 3000;
-=======
 const port = process.env.PORT || 3000;
->>>>>>> dev
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
